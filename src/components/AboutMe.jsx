4b6a6ccb15bf2ba6c0b00eebf0fe37d91a5e492a
Rohit Mulange <rohit.mulange@flipkart.com>
2022-01-26 23:32:32 +0530

    Flipkart change

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,7 +14,7 @@ export function AboutMe(props) {
         I am a Software Engineer with 6+ years of experience building Web applications.
         I am passionate about using cutting edge modern technologies to build beautiful, interactive and  high performance User Interfaces. 
         I like contributing to open source and am a proud <strong>Google Summer of Code 2015</strong> alumni, and <strong>Google Summer of Code 2016</strong> mentor.
-        These days, you can find me working for <strong>Thoughtworks</strong>, building amazing <strong>Microfrontends</strong>, obsessing over <strong>clean code</strong> and advocating for <strong>Web Accessibility</strong>.
+        These days, you can find me working for <strong>Flipkart</strong>, building amazing <strong>UI</strong>, obsessing over <strong>clean code</strong> and advocating for <strong>Web Accessibility</strong>.
       </Paragraph>
     </Container>
   );
